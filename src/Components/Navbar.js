277c3a3464874ea36f7bc40854b5481e10a05841
Jakvax012 <valek11@stud.uniza.sk>
2024-09-05 14:33:07 +0200

    Added: top-bar content, links, testing mobile version and repair bugs

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,5 @@
 import { useRef } from 'react';
-import { FaBars, FaTimes } from 'react-icons/fa';
+import { FaBars, FaTimes, FaPhone, FaEnvelope, FaUser } from 'react-icons/fa';
 import "../styles/style-header.css";
 
 function Navbar() {
@@ -11,28 +11,40 @@ function Navbar() {
 
     return (
         <header>
-            <div className="top-bar"></div>
+            <div className="top-bar">
+                <div className="content">
+                    <div>
+                        <a href="tel:+420 777 444  063 " className="phone-number-top-menu"><FaPhone/> +420 777 444  063 </a>
+                        <a href="mailto:hotline@smartbooks.sk" className="mobile-hidden"><FaEnvelope/> hotline@smartbooks.sk</a>
+                    </div>
+                    <div>
+                        <a href="https://ucenie.smartbooks.sk/" className="mobile-hidden"><FaUser/> prihlásiť / registrovať sa</a>
+                    </div>
+                </div>
+            </div>
             <div className="main-bar">
-                <a href='/#' className='logo-header-link'>
-                    <img 
-                    src='https://www.smartbooks.sk/wp-content/uploads/2024/09/logo-new-1536x330-1.png'
-                    alt='Logo smartbooks.sk'
-                    className='logo-header'/>
-                </a>
-                <nav ref={navRef}>
-                    <a href="/#">Na doma</a>
-                    <a href="/#">Pre školy</a>
-                    <a href="/#">Smart učenie</a>
-                    <a href="/#">Predmety</a>
-                    <a href="/#">Podpora</a>
-                    <a href="/#">Cenník</a>
-                    <button className='nav-btn nav-close-btn' onClick={showNavbar}>
-                        <FaTimes />
+                <div className="content">
+                    <a href='https://www.smartbooks.sk/' className='logo-header-link'>
+                        <img 
+                        src='https://www.smartbooks.sk/wp-content/uploads/2024/09/logo-new-1536x330-1.png'
+                        alt='Logo smartbooks.sk'
+                        className='logo-header'/>
+                    </a>
+                    <nav ref={navRef}>
+                        <a href="https://www.smartbooks.sk/rodicia/">Pre rodičov</a>
+                        <a href="https://www.smartbooks.sk/ucitelia/">Pre školy</a>
+                        <a href="https://www.smartbooks.sk/co-je-to-smartbooks/">Čo je to Smartbooks</a>
+                        <a href="https://www.smartbooks.sk/kontakty/">Kontakty</a>
+                        <a href="https://www.smartbooks.sk/podpora/">Podpora</a>
+                        <a href="https://ucenie.smartbooks.sk/" className="desktop-hidden">Prihlásiť / Registrovať sa</a>
+                        <button className='nav-btn nav-close-btn' onClick={showNavbar}>
+                            <FaTimes />
+                        </button>
+                    </nav>
+                    <button className='nav-btn' onClick={showNavbar}>
+                        <FaBars />
                     </button>
-                </nav>
-                <button className='nav-btn' onClick={showNavbar}>
-                    <FaBars />
-                </button>
+                </div>
             </div>
         </header>
     );
